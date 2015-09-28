534d44fb54181f4cc2b27e1b591eabea0e67d663
Fedor Indutny <fedor@indutny.com>
2015-09-28 00:26:09 -0400

    combo: tests, ssa:phi reduction

--- Makefile
+++ Makefile
@@ -1,15 +1,25 @@
 JSCS ?= node_modules/.bin/jscs
 JSHINT ?= node_modules/.bin/jshint
+MOCHA ?= node_modules/.bin/mocha
 
 SRC ?=
 SRC += $(shell find lib/ -name '*.js')
 TEST ?=
-TEST += $(shell find test/ -name '*.js')
+TEST += $(shell find test/ lib/ -name '*-test.js')
+GREP ?=
+
+test:
+	$(MOCHA) --reporter=spec $(TEST)
+	make lint
+
+test-grep:
+	$(MOCHA) --reporter=spec $(TEST) --grep "$(GREP)"
+	make lint
 
 lint:
 	@echo Checking style
-	@$(JSCS) $(SRC) $(TEST)
+	@$(JSCS) $(SRC)
 	@echo Checking stupid mistakes
 	@$(JSHINT) $(SRC)
 
-.PHONY: lint
+.PHONY: lint test
--- test/fixtures.js
+++ test/fixtures.js
@@ -2,6 +2,10 @@
 
 var assertText = require('assert-text');
 assertText.options.trim = true;
+
+var pipeline = require('json-pipeline');
+var Reducer = require('json-pipeline-reducer');
+var Scheduler = require('json-pipeline-scheduler');
 var disasm = require('disasm');
 var fixtures = require('./fixtures');
 
@@ -19,4 +23,18 @@ exports.testAsm = function testAsm(input, expected) {
   asm = disasm.stringify(asm);
 
   assertText.equal(asm, exports.fn2str(expected));
+};
+
+exports.testReduction = function testReduction(reduction, input, expected) {
+  var p = pipeline.create('cfg');
+  p.parse(exports.fn2str(input), { cfg: true }, 'printable');
+
+  var reducer = Reducer.create();
+  reducer.addReduction(reduction);
+  reducer.reduce(p);
+
+  var scheduled = Scheduler.create(p).run();
+  scheduled.reindex();
+  assertText.equal(scheduled.render({ cfg: true }, 'printable'),
+                   exports.fn2str(expected));
 };

3580b5fd5f101ab1a3660d86465814f9c9557d20
Fedor Indutny <fedor@indutny.com>
2015-10-07 16:53:20 -0400

    test: i64.eq in branch

--- test/x64/math-test.js
+++ test/x64/math-test.js
@@ -71,6 +71,28 @@ describe('wasm Compiler/x64/math', function() {
       */});
     });
 
+    it('should support i64.eq in branch', function() {
+      testAsm(function() {/*
+        i64 main(i64 a, i64 b) {
+          if (i64.eq(a, b))
+            return a;
+          else
+            return b;
+        }
+      */}, function() {/*
+        push rbp
+        mov rbp, rsp
+        cmp rsi, rdx
+        far-jcc nz, 0x8
+        mov rax, rsi
+        jmp 0x3
+        mov rax, rdx
+        mov rsp, rbp
+        pop rbp
+        ret
+      */});
+    });
+
     it('should support i64.ne', function() {
       testAsm(function() {/*
         i64 main(i64 a) {

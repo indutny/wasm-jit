191640817047ded494036b75a2161ffec9458ae2
Fedor Indutny <fedor@indutny.com>
2015-10-12 23:08:54 -0400

    test: style fixes

--- test/api-test.js
+++ test/api-test.js
@@ -112,6 +112,7 @@ describe('wasm Compiler/API', function() {
         i32.store(addr.from_64(i64.const(0)), i32.const(0x6c6c6568));
         i32.store(addr.from_64(i64.const(4)), i32.const(0x6177206f));
         i32.store(addr.from_64(i64.const(8)), i32.const(0x000a6d73));
+
         std::print(addr.from_64(i64.const(0)),
                    addr.from_64(i64.const(11)));
       }
